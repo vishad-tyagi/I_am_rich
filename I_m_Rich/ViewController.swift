1aaadb360123c113edd9b20f62a780a28fb43809
vishad-tyagi <78733360+vishad-tyagi@users.noreply.github.com>
2022-06-01 06:59:07 +0530

    Initial Commit

---
+++ I_m_Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I_m_Rich
+//
+//  Created by Vishad Tyagi on 01/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
